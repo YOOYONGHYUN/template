a831c21e00dc9200919fddcabdf83ea23a7fd7d0
YOOYONGHYUN <yyh901@naver.com>
2022-07-19 13:35:51 +0900

    user-friendly

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 // do something!
+if (localStorage.getItem("active") === "true") {
+  document.querySelector("nav").classList.add("active");
+  document.body.style.visibility = "visible";
+} else (localStorage.getItem("active") === "false") {
+  document.body.style.visibility = "visible";
+}
+
 let tg = document.querySelector("i");
 
 tg.addEventListener("click", function () {
   document.querySelector("nav").classList.toggle("active");
+  if (document.querySelector("nav").classList.contains("active") === true) {
+    localStorage.setItem("active", "true");
+  } else {
+    localStorage.setItem("active", "false");
+  }
 });
 
 window.addEventListener("load", function () {
