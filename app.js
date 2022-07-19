fc238c5c4c53025f7ba5a7eaa69cce3318157d3a
YOOYONGHYUN <yyh901@naver.com>
2022-07-19 09:39:34 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // do something!
+let tg = document.querySelector("i");
+
+tg.addEventListener("click", function () {
+  document.querySelector("nav").classList.toggle("active");
+});
+
+window.addEventListener("load", function () {
+  document.body.classList.remove("preload");
+});
