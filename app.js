fc7e114de5ab1312d7c22f960d8472ae182ba91d
YOOYONGHYUN <yyh901@naver.com>
2022-08-06 12:37:51 +0900

    주석달기

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // do something!
+//localstorage에 저장된 상태에 따라 visibility 변경
 if (localStorage.getItem("active") === "true") {
   document.querySelector("nav").classList.add("active");
   document.body.style.visibility = "visible";
@@ -7,7 +8,7 @@ if (localStorage.getItem("active") === "true") {
 }
 
 let tg = document.querySelector("i");
-
+//toggle-side & localstoage에 상태 저장
 tg.addEventListener("click", function () {
   document.querySelector("nav").classList.toggle("active");
   if (document.querySelector("nav").classList.contains("active") === true) {
@@ -17,6 +18,7 @@ tg.addEventListener("click", function () {
   }
 });
 
+//preload
 window.addEventListener("load", function () {
   document.body.classList.remove("preload");
 });
